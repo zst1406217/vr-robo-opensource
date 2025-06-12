e1d8f94badf9498579b2b6398bd369671f73daa0
Shaoting Zhu <zhust24@mails.tsinghua.edu.cn>
2025-06-12 21:43:47 +0800

    fix config not found bug

--- vrrobo_renderer/arguments/__init__.py
+++ vrrobo_renderer/arguments/__init__.py
@@ -122,16 +122,6 @@ def get_combined_args(parser : ArgumentParser):
     cmdlne_string = sys.argv[1:]
     cfgfile_string = "Namespace()"
     args_cmdline = parser.parse_args(cmdlne_string)
-
-    try:
-        cfgfilepath = os.path.join(args_cmdline.model_path, "cfg_args")
-        print("Looking for config file in", cfgfilepath)
-        with open(cfgfilepath) as cfg_file:
-            print("Config file found: {}".format(cfgfilepath))
-            cfgfile_string = cfg_file.read()
-    except TypeError:
-        print("Config file not found at")
-        pass
     args_cfgfile = eval(cfgfile_string)
 
     merged_dict = vars(args_cfgfile).copy()
